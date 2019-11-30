55940f1638dcc0062e0ed6c30e7a86e7f7ec5f49
Alamgir Qazi <alamgirmunirqazi@gmail.com>
2019-11-30 13:57:05 +0500

    added jwt + bcrypt

--- index.js
+++ index.js
@@ -2,88 +2,127 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const app = express();
-mongoose.connect('mongodb://localhost:27017/myfirstmongodb', {useNewUrlParser: true,useUnifiedTopology:true});
+mongoose.connect('mongodb://localhost:27017/booky', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+const bcrypt = require('bcryptjs');
+const jsonwebtoken = require('jsonwebtoken');
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
-const Student = mongoose.model('Student', { 
-    name: String,
-    student_id: Number,
-    email: String,
-    password: String,
-    date_added: Date
-   });
+app.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
 
-app.get('/', (req, res) => { 
+const Users = mongoose.model('Users', {
+  name: String,
+  email: String,
+  password: String,
+  date_added: Date
+});
+
+const Books = mongoose.model('Books', {
+  name: String,
+  author: String,
+  ibn_number: String,
+  image_url: String,
+  date_added: Date
+});
+
+app.get('/', (req, res) => {
   res.send('Welcome to my Node.js app');
 });
 
-app.get('/students', async (req, res) => { 
-
+app.get('/students', async (req, res) => {
   const allStudents = await Student.find();
   console.log('allStudents', allStudents);
 
   res.send(allStudents);
 });
 
- app.post('/signup', async (req, res) => { 
-const body = req.body;
-console.log('req.body', body)
+app.post('/signup', async (req, res) => {
+  try {
+    const body = req.body;
 
-const student = new Student(body);
+    // there must be a password in body
 
-const result = await student.save();
+    // we follow these 2 steps
 
-res.send({
-  message: 'Student signup successful'
+    const password = body.password;
+
+    var salt = bcrypt.genSaltSync(10);
+    var hash = bcrypt.hashSync(password, salt);
+
+    body.password = hash;
+    console.log('hash - > ', hash);
+    const user = new Users(body);
+
+    const result = await user.save();
+
+    res.send({
+      message: 'Student signup successful'
+    });
+  } catch (ex) {
+    console.log('ex', ex);
+
+    res
+      .send({
+        message: 'Error',
+        detail: ex
+      })
+      .status(500);
+  }
 });
 
-  });
-
-  app.post('/login',  async (req, res) => { 
+app.post('/login', async (req, res) => {
+  try {
     const body = req.body;
-    console.log('req.body', body);
 
     const email = body.email;
 
-    // lets check if email exists 
+    // lets check if email exists
 
-    const result = await Student.findOne({"email":  email});
-    if(!result) // this means result is null
-    {
+    const result = await Users.findOne({ email: email });
+    if (!result) {
+      // this means result is null
       res.status(401).send({
         Error: 'This user doesnot exists. Please signup first'
-       });
-    }
-    else{
-      // email did exist 
-      // so lets match password 
+      });
+    } else {
+      // email did exist
+      // so lets match password
 
-      if(body.password === result.password){
-
-        // great, allow this user access 
+      if ( bcrypt.compareSync(body.password, result.password)) {
+        // great, allow this user access
 
         console.log('match');
+        
+        delete result['password'];
+        
+        const token = jsonwebtoken.sign({
+           data: result,
+           role: 'User'
+        }, 'supersecretToken', { expiresIn: '7d' });
+        
+        console.log('token -> ', token)
 
-        res.send({message: 'Successfully Logged in'});
+        res.send({ message: 'Successfully Logged in', token: token });
+      } 
+      
+      else {
+        console.log('password doesnot match');
+
+        res.status(401).send({ message: 'Wrong email or Password' });
       }
-
-        else{
-
-          console.log('password doesnot match');
-
-          res.status(401).send({message: 'Wrong email or Password'});
-        }
-
-
     }
+  } catch (ex) {
+    console.log('ex', ex);
+  }
+});
 
-  });
-
-
-app.get('*', (req, res) => { 
+app.get('*', (req, res) => {
   res.send('Page doesnot exists');
 });
 
